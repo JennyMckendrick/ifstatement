e85873a4aec2611632fb8cf83289add55ee599ed
JennyMckendrick <117669951+JennyMckendrick@users.noreply.github.com>
2022-11-11 10:56:43 +0000

    Add files via upload

--- js/sketch.js
+++ js/sketch.js
@@ -1,11 +1,20 @@
 function setup() {
-    var canvas = createCanvas(800, 800);
+    var canvas = createCanvas(400, 400);
     canvas.parent("p5container");
 }
 
 function draw() {
     background(220);
 
-    fill(255, 255, 255);
-    ellipse(400, 400, 200);
+    if(mouseX > 0 && mouseX < width/2) {
+    //left - Smiley face
+    ellipse(mouseX > 0 && mouseX < width/2, 50, 50);
+
+
+    }else{
+        //right - Frowny face
+        fill(255,0,0);
+        ellipse(mouseX > 0 && mouseX < width/2, 50, 50);
+    }
+
 }
